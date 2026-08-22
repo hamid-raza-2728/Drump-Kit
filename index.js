eac84b85d8208dcd1cfeb65a15e586839a3a97dc
MD Hamid Raza <shaikhhamidraza98710@gmail.com>
2026-08-22 14:28:15 +0530

    Fix kick sound file name in drum event handler
    Updated the kick sound file to 'kick-bass.mp3'.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var crash=new Audio("./sounds/crash.mp3");
 crash.play();
 break; 
 case "l": 
-var kick=new Audio("./sounds/kick.mp3");
+var kick=new Audio("./sounds/kick-bass.mp3");
 kick.play();
 break;  
 default :
